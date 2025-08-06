f64cabbd18c30848e1d0081c638075ba9d74e905
dracconi <kacmalina@gmail.com>
2025-08-06 14:58:07 +0200

    import ips

--- top/init.tcl
+++ top/init.tcl
@@ -1,4 +1,5 @@
-create_project -force top prj
+create_project -force top prj -part xc7s50csga324-1
 add_files -fileset sim_1 sim
 add_files -fileset constrs_1 constr
 add_files src
+import_ip ips/xfft_forward/xfft_forward.xci ips/xfft_reverse/xfft_reverse.xci
