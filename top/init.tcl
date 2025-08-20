612d067d886d8548b7eb486fd198e93832c3eda9
dracconi <kacmalina@gmail.com>
2025-08-20 02:52:41 +0200

    add docs

--- top/init.tcl
+++ top/init.tcl
@@ -4,7 +4,7 @@
 #add_files src
 #source bd.tcl
 if {[file exist "prj"]} {
-    open_project -part xc7s50csga324-1 "prj/prj.xpr"
+    open_project -part xc7s50csga324-1 "prj/top.xpr"
 } else {
     create_project -force top prj -part xc7s50csga324-1
     add_files -fileset sim_1 sim
