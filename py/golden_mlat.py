9eb513581334888f661d3ece6e34ee34448bb3fa
dracconi <kacmalina@gmail.com>
2025-08-26 02:03:18 +0200

    make the discrimination rules a bit different

--- py/golden_mlat.py
+++ py/golden_mlat.py
@@ -4,11 +4,12 @@ import random
 # w.l.o.g. ref mic = (0, 0, 0)
 
 REF = np.array([0, 0, 0])
-SENSORS = np.array([[1, 0, 0], [-0.77, -0.77, -2], [-0.77, 0.77, 1]])*5
+SENSORS = np.array([[1, 0, 0], [-0.77, -0.77, -0.5], [-0.77, 0.77, 1]])*10
 ITERATIONS = 500
-EPS = 100
+EPS = 10
 
 MINIMUM_DISTANCE_LATERAL = 100
+CENTER_DISTANCE = 300
 
 def get_rel_dist(ref, source, sensor):
     nref = np.linalg.norm(source-ref)
@@ -17,7 +18,6 @@ def get_rel_dist(ref, source, sensor):
 
 def get_dist(ref, to):
     return np.linalg.norm(to-ref)
-
 
 def mlat_sx(minv, rmics, darr):
     """Perform MLAT based on Schau&Robinson 1987 paper
@@ -44,12 +44,19 @@ def mlat_sx(minv, rmics, darr):
     
 
 def mlat_sx_easy(src):
-    darr = [get_rel_dist(REF, src, sens) + random.randrange(-5, 5)/10000.0 for sens in SENSORS]
+    darr = [get_rel_dist(REF, src, sens) + random.randrange(-5, 5)/2000.0 for sens in SENSORS]
     res = mlat_sx(np.linalg.inv(SENSORS), [get_dist(REF, sens) for sens in SENSORS], darr)
     
     #assert (res > MINIMUM_DISTANCE_LATERAL).all(axis=0)[2] == False, f"Ooops.. could not determine the result {src} {res}"
+
+    dist = [0, 0]
     
-    if res[0][2] > res[1][2]:
+    for i in range(2):
+        dist[i] = abs(get_dist(REF, res[i]) - CENTER_DISTANCE)
+        if res[i][2] < 0:
+            dist[i] = 9999999
+        
+    if dist[0] < dist[1]:
         pos = res[0]
     else:
         pos = res[1]
@@ -58,6 +65,7 @@ def mlat_sx_easy(src):
 
     if ok == False:
         print("\n\n-- ERROR BIGGER THAN EPS!")
+        print(dist)
         print("res")
         print(res)
         print("src")
@@ -79,7 +87,7 @@ def main():
             count_ok = count_ok + 1
         print(".", end="")
     print("")
-    print("Total %d, ok %d, %f%% rate" % (ITERATIONS, count_ok, count_ok/ITERATIONS))
+    print("Total %d, ok %d, %f%% rate" % (ITERATIONS, count_ok, 100*count_ok/ITERATIONS))
 
 
 if __name__ == "__main__":
