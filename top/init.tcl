4ae00d815eb4a83e8f219e38dd422a4aea6396d8
dracconi <kacmalina@gmail.com>
2025-08-31 14:48:27 +0200

    update init.tcl to be reproducible

--- top/init.tcl
+++ top/init.tcl
@@ -1,8 +1,3 @@
-#create_project -force top prj -part xc7s50csga324-1
-#add_files -fileset sim_1 sim
-#add_files -fileset constrs_1 constr
-#add_files src
-#source bd.tcl
 if {[file exist "prj"]} {
     open_project -part xc7s50csga324-1 "prj/top.xpr"
 } else {
@@ -18,6 +13,12 @@ if {[file exist "prj"]} {
     update_ip_catalog -rebuild
 
     create_ip -vlnv polsl:gcc_internal:gcc_phat:1.0 -module_name gcc_phat_0
-    
-    #source recreate.tcl
+
+    read_bd bd/demo/demo.bd
+    read_bd bd/test_uart/test_uart.bd
+
+    make_wrapper -top -import [get_files bd/demo/demo.bd]
+    make_wrapper -top -import [get_files bd/test_uart/test_uart.bd]
+
+    set_property top demo_wrapper [current_fileset]
 }
